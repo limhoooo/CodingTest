e6e14bbf1ce3b72d02f87983d4479238c728176d
lim ho <dlagh123@gmail.com>
2023-07-14 11:26:24 +0900

    [unrated] Title: 추억 점수, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -4,16 +4,25 @@ function solution(name, yearning, photo) {
       obj[key] = yearning[index];
     });
     
-    var answer = [];
-    for(let i=0; i < photo.length; i++){
-        let count = 0;
-        for(let j=0; j < photo[i].length; j++){
-            if(obj[photo[i][j]] !== undefined) {
-                count = count + obj[photo[i][j]];
-            }
-        }
-        answer.push(count)
-    }
+  const answer = photo.map((row) => {
+    let count = 0;
+    row.forEach((item) => {
+      if (obj[item] !== undefined) {
+        count += obj[item];
+      }
+    });
+    return count;
+  });
+    
+    // for(let i=0; i < photo.length; i++){
+    //     let count = 0;
+    //     for(let j=0; j < photo[i].length; j++){
+    //         if(obj[photo[i][j]] !== undefined) {
+    //             count = count + obj[photo[i][j]];
+    //         }
+    //     }
+    //     answer.push(count)
+    // }
 
     return answer;
 }
