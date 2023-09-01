9a828f8c4cbe8200142c21ec5a9685bf02d6cba1
lim ho <dlagh123@gmail.com>
2023-09-01 16:44:17 +0900

    [Bronze V] Title: A×B, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.js
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var input = fs.readFileSync('/dev/stdin').toString().split('\n');
 var data = input[0].split(' ')
-var a = parseInt(data[0]);
-var b = parseInt(data[1]);
+
+
+var a = Number(data[0]);
+var b = Number(data[1]);
 console.log(a * b);
