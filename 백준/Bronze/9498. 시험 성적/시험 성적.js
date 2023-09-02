5db1a068ad023feb38e9327d677d20a86450ae7d
lim ho <dlagh123@gmail.com>
2023-09-02 12:57:36 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const a = parseInt(input[0]);
+if (a >= 90 && a <= 100) {
+  console.log("A");
+} else if (a >= 80 && a <= 89) {
+  console.log("B");
+} else if (a >= 70 && a <= 79) {
+  console.log("C");
+} else if (a >= 60 && a <= 69) {
+  console.log("D");
+} else {
+  console.log("F");
+}
