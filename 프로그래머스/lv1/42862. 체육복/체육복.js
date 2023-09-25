140885dcc3b917b84c9578571c2aa903b2d78dc3
lim ho <dlagh123@gmail.com>
2023-09-25 15:16:43 +0900

    [level 1] Title: 체육복, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,37 +1,34 @@
-function solution(n, lost, reserve) {
-    let answer = 0;
-    lost = lost.sort((a, b) => a - b);    
-    reserve = reserve.sort((a, b) => a - b);
+// reserve 의 값 앞뒤로만 빌려줄수있음 4 = 3,5
+// 맨앞 맨뒤 예외처리 해야함
+// lost reserve 에 중복된 숫자있을수있음
 
-    for(let i = 1; i <= n; i++) {
-        //만약 i가 lost 배열에 없다면(즉, 옷이 있다면)
-        if (lost.indexOf(i) < 0) {
-            answer++;
-        }
-        //만약 i가 lost 배열에 있다면(즉, 옷이 없다면)
-        else {
-            //만약 직전사람이 여분 옷이 있다면
-            if (reserve.indexOf(i - 1) >= 0 && lost.indexOf(i-1) < 0) {
-                answer++;
-                //여분 옷 썼으니까 reserve 배열에서 제거
-                reserve = reserve.filter((element) => element !== (i-1));
-            } 
-            //직전 사람은 옷이 없다는데 혹시 다음사람이 여분옷이 있다면? (단, 여분옷있는 사람이 lost 배열에 있으면 안됨)
-            else if (reserve.indexOf(i + 1) >= 0 && lost.indexOf(i+1) < 0) {
-                answer++;
-                //여분 옷 썼으니까 reserve 배열에서 제거
-                reserve = reserve.filter((element) => element !== (i+1));
-            }
-            //만약 자기가 도난도 당하고 여분 옷도 있다면? (즉, lost와 reserve 배열에 모두 포함)
-            else if (reserve.indexOf(i) >= 0) {
-                answer++;
-                //여분 옷 썼으니까 reserve 배열에서 제거
-                reserve = reserve.filter((element) => element !== (i));
-            }
-            else {
-                continue;
-            }
-        }
+function solution(n, lost, reserve) {
+  let result = 0;
+  result = n - lost.length;
+  let lostList = lost.sort((a, b) => a - b);
+  let reserveList = reserve.sort((a, b) => a - b);
+
+  for (let i = 0; i < lostList.length; i++) {
+    for (let j = 0; j < reserveList.length; j++) {
+      if (lostList[i] === reserveList[j]) {
+        result++;
+        lostList[i] = -1;
+        reserveList[j] = -1;
+      }
     }
-    return answer;
+  }
+  for (let i = 0; i < reserveList.length; i++) {
+    if (reserveList[i] === -1) continue;
+    const plus = reserveList[i] + 1;
+    const minus = reserveList[i] - 1;
+    if (lostList.includes(minus)) {
+      result++;
+      lostList = lostList.filter(item => item !== minus);
+    } else if (lostList.includes(plus)) {
+      result++;
+      lostList = lostList.filter(item => item !== plus);
+    }
+  }
+  return result;
 }
+
