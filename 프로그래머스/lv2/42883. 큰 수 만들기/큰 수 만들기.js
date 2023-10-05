fef3275a51b723e60d4fb1d8f759c098b8c88c46
lim ho <dlagh123@gmail.com>
2023-10-05 15:54:23 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,19 +1,15 @@
 function solution(number, k) {
-    const stack = []; // 큰 숫자를 저장할 스택
-    for (let digit of number) {
-        while (k > 0 && stack.length > 0 && stack[stack.length - 1] < digit) {
-            // 스택의 마지막 숫자가 현재 숫자보다 작고, 아직 제거해야 할 숫자가 남아있을 때
-            stack.pop(); // 스택에서 마지막 숫자를 제거
-            k--; // 제거한 숫자 개수 감소
-        }
-        stack.push(digit); // 현재 숫자 스택에 추가
+  const stack = []; // 큰 숫자를 저장할 스택
+  for (let num of number) {
+    while (k > 0 && stack.length > 0 && stack[stack.length - 1] < num) {
+      stack.pop();
+      k--;
     }
-    
-    // 남은 k개의 숫자 제거
-    while (k > 0) {
-        stack.pop();
-        k--;
-    }
-    
-    return stack.join(''); // 스택에 남은 숫자들을 문자열로 결합하여 반환
+    stack.push(num);
+  }
+  while (k > 0) {
+    stack.pop();
+    k--;
+  }
+  return stack.join(''); // 스택에 남은 숫자들을 문자열로 결합하여 반환
 }
