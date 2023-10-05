0985ac5837e5f97502ca1bfc195431ef92584406
lim ho <dlagh123@gmail.com>
2023-10-05 16:13:28 +0900

    [level 2] Title: 구명보트, Time: 13.02 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -0,0 +1,15 @@
+
+function solution(people, limit) {
+  const descPeople = people.sort((a,b)=>b-a)
+  let boatCount = 0
+  descPeople.forEach(people=>{
+      if(people + descPeople.at(-1) > limit){
+          boatCount++
+      }
+      else{
+          descPeople.pop()
+          boatCount++
+      }
+  })
+  return boatCount
+}
