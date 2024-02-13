b9352cdb50f7baac72f9ac18c208c070920839bd
lim ho <dlagh123@gmail.com>
2024-02-13 13:20:06 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,16 +1,13 @@
 function solution(cards1, cards2, goal) {
-    let result = ''
-    for(let i=0;i<goal.length;i++){
+    let result = 'Yes'
+    for(let i=0; i<goal.length; i++){
         if(goal[i] === cards1[0]){
             cards1.shift()
         }else if(goal[i] === cards2[0]){
             cards2.shift()
-        }
-        else {
-            return result = 'No'
+        }else{
+            result = 'No'
         }
     }
-    
-    return result = result ? 'no' : 'Yes'
-    
+    return result;
 }
