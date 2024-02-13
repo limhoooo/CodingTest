1b504d75f1be4d5e1fbb050ef840276516ffd6fc
lim ho <dlagh123@gmail.com>
2024-02-13 22:24:51 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,34 +1,31 @@
 function solution(sequence, k) {
     var answer = [];
-    let prefix = [0]       // sequence의 합을 나타낸 배열
-    let maxL  = Infinity   // 정답으로 나온 부분수열의 길이를 나타내는 변수 
-    sequence.forEach((num,i)=>{
-        prefix.push(num+prefix[i])
-    })
+    
+    
+    let end = 0;
+    let sum = 0;
 
-    // prefix[i]에는 sequence[0]부터 sequence[i-1] 요소까지의 합이 들어있다.
-    // ex) sequence = [1, 2, 3, 4, 5] , prefix = [0, 1, 3, 6, 10, 15]
-
-    let left = 0
-    let right = 0
-
-    // 투포인터 시작. 
-    // 현재까지의 합이 K보다 작으면 right를 +1씩 올린다.
-    // 아니라면 left를 +1씩 올린다.
-    while(left<=right){
-        let sum = prefix[right] - prefix[left]  // sequence[left] 부터 sequence[right-1]까지의 수열의 합.
-        if(sum === k) {
-            // 정답수열을 찾았을때 수열의 길이를 체크하고 가장 작은길이의 수열로 변환. 
-            let nowL = right-1 - left 
-            if(maxL > nowL){
-                answer = [left,right-1]
-                maxL = nowL
-            }
+    for(let start=0; start<sequence.length; start++){
+        while(k > sum && end < sequence.length){
+            sum = sum + sequence[end]
+            end++;
         }
-        if(sum<k){
-            right++
-        }else left++
+        if(sum === k) answer.push([start,end-1]);
+        sum -= sequence[start]
     }
-
+    
+    if(answer.length > 0) {
+        let calc = answer[0][1] - answer[0][0];
+        let result = [answer[0][0], answer[0][1]];
+        for(let i=0; i<answer.length; i++){
+           if(calc > answer[i][1] - answer[i][0]){
+               calc = answer[i][1] - answer[i][0];
+               result = [answer[i][0], answer[i][1]];
+           } 
+        }
+        return result;
+    }
+        
+    
     return answer;
 }
