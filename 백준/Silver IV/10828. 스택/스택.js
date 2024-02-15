5a4b6f453366a3edc20ecbcd15467d36d9bd4b06
lim ho <dlagh123@gmail.com>
2024-02-15 19:44:13 +0900

    [Silver IV] Title: 스택, Time: 200 ms, Memory: 12752 KB -BaekjoonHub

--- 백준/Silver IV/10828. 스택/스택.js
+++ 백준/Silver IV/10828. 스택/스택.js
@@ -4,53 +4,38 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-const n = Number(input[0]);
-const commands = input.slice(1);
+const N = Number(input[0]);
+const arr = input.slice(1);
 
-// 문제 풀이
-function solution(n, commands) {
-	const stack = [];
-	let top = 0;
-	let answer = '';
-
-	for (let i = 0; i < n; i += 1) {
-		const command = commands[i].split(' ')[0];
-		let result = '';
-
-		switch (command) {
-			case 'push':
-				const pushItem = commands[i].split(' ')[1];
-				stack[top++] = pushItem;
-				break;
-			case 'pop':
-				if (top) {
-					top -= 1;
-					result = stack.splice(-1);
-					answer += result + ' ';
-				} else {
-					result = -1;
-					answer += result + ' ';
-				}
-				break;
-			case 'top':
-				result = top ? stack[top - 1] : -1;
-				answer += result + ' ';
-				break;
-			case 'empty':
-				result = top ? 0 : 1;
-				answer += result + ' ';
-				break;
-			case 'size':
-				result = top;
-				answer += result + ' ';
-				break;
-			default:
-				break;
-		}
-	}
-
-	console.log(answer.split(' ').join('\n'));
+let stack = [];
+let result = "";
+for (let i = 0; i < arr.length; i++) {
+  let command = arr[i].split(" ")[0].trim();
+  let txt = "";
+  switch (command) {
+    case "push":
+      stack.push(arr[i].split(" ")[1]);
+      break;
+    case "top":
+      txt = stack.length > 0 ? stack[stack.length - 1] : "-1";
+      result += txt.trim() + " ";
+      break;
+    case "size":
+      txt = stack.length;
+      result += txt + " ";
+      break;
+    case "empty":
+      txt = stack.length > 0 ? "0" : "1";
+      result += txt + " ";
+      break;
+    case "pop":
+      txt = stack.splice(-1);
+      let tet2 = txt.length > 0 ? txt + "" : "-1";
+      result += tet2.trim() + " ";
+      break;
+    default:
+      break;
+  }
 }
 
-// 제출
-solution(n, commands);
+console.log(result.split(" ").join("\n").trim());
